4a3ac13832f8ccc7ef38f6c3d7da3b2474f7c4da
thangvu2325 <99258025+thangvu2325@users.noreply.github.com>
2023-05-09 20:41:17 +0700

    Update Nhap_Diem.cs

--- Final_Project_WinForm/Form_CS/Nhap_Diem.cs
+++ Final_Project_WinForm/Form_CS/Nhap_Diem.cs
@@ -26,6 +26,7 @@ namespace Final_Project_WinForm
 
             Nhap_Lop.DS_Lop.getSinhvien(listView_ds_Diem,malop, maMH, int.Parse(lanThi)); 
             
+            
         }
 
         private void btn_add_Click(object sender, EventArgs e)
@@ -39,7 +40,8 @@ namespace Final_Project_WinForm
                 string maMH = txt_maMH.Text;
                 string lanThi = txt_LanThi.Text;
                 Nhap_Lop.DS_Lop.getSinhvien(listView_ds_Diem, malop, maMH, int.Parse(lanThi), MSSV_upDiem, int.Parse(txt_diem.Text));
-
+                Main_Form.colListClass.DeleteMany(Builders<LinkedList_Lop>.Filter.Empty);
+                Main_Form.colListClass.InsertOne(Nhap_Lop.DS_Lop);
             }
             txt_diem.ResetText();
         }
@@ -51,6 +53,7 @@ namespace Final_Project_WinForm
                 ListViewItem lvi = listView_ds_Diem.SelectedItems[0];
                 lab_mssv.Text = lvi.SubItems[1].Text;
                 MSSV_upDiem = lvi.SubItems[1].Text;
+              
             }
         }
     }
